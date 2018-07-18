19ba4e44a9670fb9c0e9a14707682b32e24f6ff3
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-07-17 23:57:07 -0400

    WTS, made twitter portion of liri operational so it calls back tweets from my account, added spotify keys to env file, then added package information to liri file, and will begin working on code to call on spotify info from text file.

--- liri.js
+++ liri.js
@@ -1,5 +1,23 @@
-js
+require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 var request = require('request');
-require("dotenv").config();
+
+var client = new Twitter(keys.twitter);
+var params = {screen_name: 'SueBuSo'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+  console.log(JSON.stringify(tweets, null, 2));
+});
+
+var spotify = new Spotify(keys.spotify);
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
