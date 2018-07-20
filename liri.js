0c7edc366bd6445b9515e1ecb258b413fc038699
Jose De Las Salas <SueBuSo@Joses-MacBook-Pro.local>
2018-07-20 16:37:39 -0400

    about two days worth of coding, forgot to upload changes made yesterday or the day before, finalized the spotify command today, Friday july 20th, working on omdbapi next, had trouble parsing response for required info in response for spotify.

--- liri.js
+++ liri.js
@@ -1,23 +1,66 @@
 require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var omdb = require('omdb');
 var keys = require("./keys.js");
 var request = require('request');
 
-var client = new Twitter(keys.twitter);
-var params = {screen_name: 'SueBuSo'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-  console.log(JSON.stringify(tweets, null, 2));
-});
+var command = process.argv[2];
+console.log(command);
 
-var spotify = new Spotify(keys.spotify);
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+//code to concatenate arguments into one process.argv
+var title = ''; 
+for (j=3; j<process.argv.length; j++) {
+  //title = title + process.argv[j] + " "
+  title += process.argv[j] + " "
+};
+
+console.log(title);
+
+//code for twitter command
+var myTweets = function() {
+  var client = new Twitter(keys.twitter);
+  var params = {screen_name: 'SueBuSo'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  
+  if (!error) {
+    for (i=0; i<tweets.length; i++) {
+      console.log("Created At: " + tweets[i].created_at);
+      console.log(" Tweet:" + tweets[i].text);
+      console.log("----------------------------");
+    }
   }
- 
-console.log(data); 
+      //console.log(JSON.stringify(tweets[0], null, 2));
 });
+};
+
+if (command === 'my-tweets') {
+  myTweets();
+};
+
+
+// spotify work begins here:
+var spotify = new Spotify(keys.spotify);
+var nameThatSong = function() {
+spotify.search({ type: 'track', query: title }, function(err, data) {
+
+  if (err) {
+
+    return console.log('Error occurred: ' + err);
+
+}
+var songInfo = data.tracks.items;
+//console.log(songInfo[0]);
+console.log("------------- Song Info -------------------");
+console.log("Artist(s): " + songInfo[0].artists[0].name);
+console.log("Song Name: " + songInfo[0].name);
+console.log("Preview Link: " + songInfo[0].preview_url);
+console.log("Album: " + songInfo[0].album.name);
+console.log("-----------------------------------------");
+});
+};
+
+if (command === 'spotify-this-song') {
+  nameThatSong();
+}
+
