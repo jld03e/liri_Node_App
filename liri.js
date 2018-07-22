79684593832cac6516b5bdfa302aa85bb0c8bb9a
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-07-21 23:14:59 -0400

    updated code to run the final requirement of the hw, now get the spotify to search, the tweets to post, the omdb to post search queries and the do this function to all work.

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var omdb = require('omdb');
 var keys = require("./keys.js");
+var fs = require('fs');
 var request = require('request');
 
 var command = process.argv[2];
@@ -40,6 +40,7 @@ if (command === 'my-tweets') {
 
 
 // spotify work begins here:
+
 var spotify = new Spotify(keys.spotify);
 var nameThatSong = function() {
 spotify.search({ type: 'track', query: title }, function(err, data) {
@@ -49,6 +50,7 @@ spotify.search({ type: 'track', query: title }, function(err, data) {
     return console.log('Error occurred: ' + err);
 
 }
+
 var songInfo = data.tracks.items;
 //console.log(songInfo[0]);
 console.log("------------- Song Info -------------------");
@@ -64,3 +66,44 @@ if (command === 'spotify-this-song') {
   nameThatSong();
 }
 
+
+//omdbapi code written here:
+var movieThis = function() {
+request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&tomatoes=true&apikey=a08a9b05", function(error, response, movies) {
+  if (!error && response.statusCode === 200) {
+    console.log("The movie's Title is: " + JSON.parse(movies).Title);
+    console.log(JSON.parse(movies).Title + " came out in: " + JSON.parse(movies).Year);
+    console.log(JSON.parse(movies).Title + "'s IMDB Rating is: " + JSON.parse(movies).imdbRating);
+    console.log(JSON.parse(movies).Title + "'s Rotten Tomatoes rating is: " + JSON.parse(movies).tomatoMeter);
+    console.log(JSON.parse(movies).Title + "'s country of production is: " + JSON.parse(movies).Country);
+    console.log(JSON.parse(movies).Title + "'s native language is: " + JSON.parse(movies).Language);
+    console.log(JSON.parse(movies).Title + "'s plot is: " + JSON.parse(movies).Plot);
+    console.log(JSON.parse(movies).Title + "'s starring actors are: " + JSON.parse(movies).Actors);
+  }
+  if(movies.length < 1) {
+    return console.log('No movies were found!');
+  }
+});
+};
+
+if (command === 'movie-this') {
+  movieThis();
+};
+
+
+//fs code starts here:
+fs.readFile("random.txt", "utf8", function(err, data) {
+
+  if (err) {
+    return console.log(err);
+  }
+
+  var dataArr = data.split(",");
+  console.log(dataArr[1]);
+
+  if (command === 'do-what-it-says') {
+    title = dataArr[1]
+    nameThatSong();
+  };
+
+  });
